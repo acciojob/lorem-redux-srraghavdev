78a78b23b2318b7dcfc974d71b4d747cdc3fd45e
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-09-01 01:00:50 +0530

    a

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,9 @@ const App = () => {
        <h1>A short Naration of Lorem Ipsum</h1>
        <h4>{'Below Contains A title and Body gotten froma random API, Please take your time to Review' }</h4>
        <ul>
-        {x.succ && x.succ.map(element=>{
+        {x.succ ? x.succ.map(element=>{
         return (<li><div className="title">Title :{element.title}</div><div className="body">Body :{element.body}</div></li>)
-       })}
+       }) : <li>'Below Contains A title and Body gotten froma random API, Please take your time to Review' </li>}
        </ul>
     </div>
   )
